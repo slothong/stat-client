fc26105cfd5a78761dec95ac4d618ee003588b2f
slothong <hep93@naver.com>
2025-06-18 22:21:09 +0900

    cdk 20으로 업그레이드

--- client/src/app/components/login-dialog-content/login-dialog-content.ts
+++ client/src/app/components/login-dialog-content/login-dialog-content.ts
@@ -3,10 +3,15 @@ import {
   HlmDialogHeaderComponent,
   HlmDialogTitleDirective,
 } from '../ui/ui-dialog-helm/src';
+import { HlmFormFieldModule } from '../ui/ui-form-field-helm/src';
 
 @Component({
   selector: 'app-login-dialog-content',
   templateUrl: './login-dialog-content.html',
-  imports: [HlmDialogHeaderComponent, HlmDialogTitleDirective],
+  imports: [
+    HlmDialogHeaderComponent,
+    HlmDialogTitleDirective,
+    HlmFormFieldModule,
+  ],
 })
 export class LoginDialogContentComponent {}
