741dce84d20de489e030bd947b20ea8615160326
slothong <hep93@naver.com>
2025-07-05 17:39:28 +0900

    remove spartan

--- src/app/components/poll-list/poll-list.ts
+++ src/app/components/poll-list/poll-list.ts
@@ -2,21 +2,12 @@ import { PollApi } from '@/services/poll-api';
 import { CommonModule } from '@angular/common';
 import { Component, computed, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { HlmCardModule } from '@ui/card';
-import { HlmRadioGroupModule } from '@ui/radio-group';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
 
 @Component({
   selector: 'app-poll-list',
-  imports: [
-    CommonModule,
-    NzCardModule,
-    NzRadioModule,
-    HlmCardModule,
-    HlmRadioGroupModule,
-    RouterLink,
-  ],
+  imports: [CommonModule, NzCardModule, NzRadioModule, RouterLink],
   templateUrl: './poll-list.html',
 })
 export class PollList {
